261f80be12733a6374f3d5db12d0e8f53cf5f18e
Egor <patrikeevegor@gmail.com>
2020-07-18 16:44:56 +0300

    get picture from back

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
+
+
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
 import { AppComponent }   from './app.component';
+import { HttpClientModule }   from '@angular/common/http';
+
+ 
+
+ 
 @NgModule({
-    imports:      [ BrowserModule, FormsModule ],
-    declarations: [ AppComponent ],
-    bootstrap:    [ AppComponent ]
+    imports:      [BrowserModule,HttpClientModule],
+    declarations: [ AppComponent],
+    bootstrap:    [ AppComponent]
 })
 export class AppModule { }
